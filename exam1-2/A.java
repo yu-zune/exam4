34f3ad541ae3cd9b7521ad9911b338184e886c7a
yu-zune <acezune@gmail.com>
2025-06-30 17:48:58 +0900

    Update A.java

--- exam1-2/A.java
+++ exam1-2/A.java
@@ -15,10 +15,11 @@ public class A {
 
 //		
 //		M Model		DB정보 관련코딩
-//					1)Service코딩  : 자바샘플
-//      			2)Mapper 코딩 : 오라클 제공 (복잡)
-//					3)VO 코딩	
-//		V View    :  JSP HTML CSS JQuery
-//		C Controller  : URL과 JSP 페이지를 연결
+//					1)Service코딩  : (비즈니스 로직)
+//      		2)Mapper 코딩 : (SQL 실행, 복잡한 쿼리 처리)
+//					3)VO 코딩	: (데이터 전달 객체)
+//		V View    :  화면 출력 담당 JSP HTML CSS JQuery
+//		C Controller  : URL과 JSP 페이지를 연결, 서비스 호출
+    
 
 }
